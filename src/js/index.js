aba7028951d03729e7a3e0c03d87a6971306d7ae
evgeniyvorobey <31759882+evgeniyvorobey@users.noreply.github.com>
2019-03-04 04:00:01 +0200

    first commit

---
+++ src/js/index.js
@@ -0,0 +1,18 @@
+window.addEventListener('load', function () {
+    // this.querySelectorAll('[data-src-img]')
+    document.querySelectorAll('[data-src-bg]').forEach(item => {
+        var url = item.getAttribute('data-src-bg');
+        item.style.background = `url(${url}) no-repeat`;
+    })
+
+    document.querySelectorAll('[data-src-img]').forEach(item => {
+        var src = item.getAttribute('data-src-img');
+        item.setAttribute('src', src);
+    })
+})
+
+if (document.querySelector('.hamburger')){
+    document.querySelector('.hamburger').addEventListener('click', function() {
+        this.classList.toggle('active')
+    })
+}
