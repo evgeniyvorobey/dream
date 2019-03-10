a16ffccfa10a12db7a9584c3feced439bcd04173
evgeniyvorobey <31759882+evgeniyvorobey@users.noreply.github.com>
2019-03-11 00:00:25 +0200

    fix

--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,29 @@
 window.addEventListener('load', function () {
-    // this.querySelectorAll('[data-src-img]')
-    document.querySelectorAll('[data-src-bg]').forEach(item => {
-        var url = item.getAttribute('data-src-bg');
-        item.style.background = `url(${url}) no-repeat`;
-    })
+
+    lazyLoadBg();
 
     document.querySelectorAll('[data-src-img]').forEach(item => {
         var src = item.getAttribute('data-src-img');
         item.setAttribute('src', src);
     })
 })
+
+
+function lazyLoadBg() {
+    document.querySelectorAll('[data-src-bg]').forEach(item => {
+        if (item.getBoundingClientRect().top < window.innerHeight ){
+            setBg(item);
+        } else {
+            document.addEventListener('scroll', function () {
+                if (item.getBoundingClientRect().top < window.innerHeight ){
+                    setBg(item);
+                }
+            })
+        }
+    })
+}
+
+function setBg(item) {
+    var url = item.getAttribute('data-src-bg');
+    item.style.background = `url(${url}) no-repeat`;
+}
